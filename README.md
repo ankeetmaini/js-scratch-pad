23e3037a02f3f6ccc7eadd1e1f8eebae34e9537c
Ankeet Maini <ankeet.maini@gmail.com>
2019-02-09 21:19:28 +0530

    Runs prettier

--- README.md
+++ README.md
@@ -1,12 +1,15 @@
 # electron-monaco-editor-sample
+
 Samples for using the monaco-editor with electron
 
 ### Install
+
 ```
 npm install
 ```
 
 ### Run
+
 ```
 npm start
 ```
--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
 /// <reference path="node_modules/monaco-editor/monaco.d.ts" />
-import * as electron from 'electron';
-import {remote} from 'electron';
+import * as electron from "electron";
+import { remote } from "electron";
 const app = remote.app;
 const BrowserWindow = remote.BrowserWindow;
 const dialog = remote.dialog;
@@ -9,28 +9,23 @@ declare var amdRequire;
 var editor: monaco.editor.IStandaloneCodeEditor;
 
 // loader.jsのrequire関数を使い、monaco-editorのコードを読み込む
-amdRequire(['vs/editor/editor.main'], () => {
+amdRequire(["vs/editor/editor.main"], () => {
   onModuleLoaded();
 });
 
 // monaco-editorのモジュール読み込み完了時に行う、エディタ初期化処理
 function onModuleLoaded() {
-    editor = monaco.editor.create(document.getElementById('container'), {
-        value: [
-        'function x() {',
-        '\tconsole.log("Hello world!");',
-        '}'
-        ].join('\n'),
-        language: 'typescript',
-        automaticLayout: true,
-        theme: "vs-dark"
-    });
+  editor = monaco.editor.create(document.getElementById("container"), {
+    value: ["function x() {", '\tconsole.log("Hello world!");', "}"].join("\n"),
+    language: "typescript",
+    automaticLayout: true,
+    theme: "vs-dark"
+  });
 }
 
 function showEditorText() {
-    var text = editor.getValue();
-    alert(text);
+  var text = editor.getValue();
+  alert(text);
 }
 
-
-document.getElementById('btnShowText').onclick = showEditorText;
+document.getElementById("btnShowText").onclick = showEditorText;
